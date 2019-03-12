8ddb5e95211501b1adbd422b00dafbe5a6630ec1
Ken Kawamoto <ken@kawamoto.co.uk>
2019-03-13 00:47:48 +0900

    Update arduino file to remove unnecessary comments.

--- Arduino/TGS2600.ino
+++ Arduino/TGS2600.ino
@@ -1,53 +1,17 @@
 void setup() {
+  // 9600bpsでシリアル通信を開始
   Serial.begin(9600);
 }
 
 void loop() {
-//  if (Serial.read() == -1) {
-//    delay(100);
-//    return;
-//  }
+  // センサはA0から入力される。値は0~1024。
   int v=analogRead(0);
-//  int capped = map(v, 0, 1024, 0, 256);
-
-
-//  Serial.write(capped);
-
-  int high = B10000000 | (v >> 5);
-  int low =  (v & B11111);
-  
-//Serial.print(v, BIN);
-////Serial.println();
-//Serial.print(" ");
-//  Serial.print(high, BIN);
-//  Serial.print(" ");
-//  Serial.print(low, BIN);
-//  Serial.println();
-  
-//Serial.println(v);
-  Serial.write(high);
-  Serial.write(low);
-  
-//  if (v>255) {
-//    v=255;
-//  }
-//  if (v<0) v=0;
-
-char buf[50];
-//sprintf(buf, "%d %d\n", highByte(v), lowByte(v));
-//Serial.println("=====");
-//Serial.println(buf);
-//  Serial.println(v);
-
-//Serial.write(highByte(v));
-//Serial.write(lowByte(v));
-
-//  Serial.write(v&0xff);
-//  Serial.write(v);
-//  Serial.println(v);
-Serial.flush();
-//  Serial.write((v>>8)&0xff);
-//  Serial.write(v&0xff);
-//  Serial.flush();
-  delay(250);
+  // 上位バイト：最上位ビットにフラグを立て、上位5ビットを下位ビットに置く。
+  Serial.write(B10000000 | (v >> 5));
+  // 下位バイト：下位5ビットだけをそのまま送る。
+  Serial.write(v & B11111);
+  // 送信が完了するまで待つ。
+  Serial.flush();
+  delay(100);
 }
+
